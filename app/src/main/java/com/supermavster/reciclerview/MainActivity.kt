c531a7fd4c0e8730979c55a79950e3f04e8d7b9a
Mavsters <mtorres@cittus.com>
2019-05-27 10:53:00 -0500

    [Add] Components Fragment And Navigation Controller

--- app/src/main/java/com/supermavster/reciclerview/MainActivity.kt
+++ app/src/main/java/com/supermavster/reciclerview/MainActivity.kt
@@ -2,11 +2,24 @@ package com.supermavster.reciclerview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.navigation.fragment.NavHostFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Init Process (All Actions in the tab MAIN)
+        initProcess()
     }
+
+    private fun initProcess() {
+        // Init
+        Log.i("Info","Init Process")
+    }
+
+    override fun onSupportNavigateUp() =
+        NavHostFragment.findNavController(nav_host_fragment).navigateUp()
 }
